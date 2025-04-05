bb736691044a5ed12a73349733b5c22e1126a4cf
Matheus Fernandes Bigolin <mfrdrbigolin@disroot.org>
2025-04-05 19:36:36 -0300

    Adiciona template incondicional de Conditionally_Data_Observed

--- include/conditionally_data_observed.hh
+++ include/conditionally_data_observed.hh
@@ -3,6 +3,7 @@
 
 #include "conditional_data_observer.hh"
 #include "ordered_list.hh"
+#include <forward_list>
 
 template <typename T, typename Condition = void>
 class Conditionally_Data_Observed {
@@ -37,6 +38,38 @@ public:
 
 private:
   Observers _observers;
+};
+
+template <typename T>
+class Conditionally_Data_Observed<T, void> {
+  friend class Conditional_Data_Observer<T, void>;
+
+public:
+  using Observer = Conditional_Data_Observer<T, void>;
+  using Observed_Data = T;
+
+public:
+  virtual void attach(Observer *o) {
+    _observers.push_front(o);
+  }
+
+  virtual void detach(Observer *o) {
+    _observers.remove(o);
+  }
+
+  virtual bool notify(Observed_Data *d) {
+    bool notified = false;
+
+    for (auto obs = _observers.begin(); obs != _observers.end(); ++obs) {
+      obs->value()->update(this, d);
+      notified = true;
+    }
+
+    return notified;
+  }
+
+private:
+  std::forward_list<Observer> _observers;
 };
 
 #endif
