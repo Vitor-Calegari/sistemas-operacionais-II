a4ede3f3959a128651e6b0fead46bb34adf7c89b
Matheus Fernandes Bigolin <mfrdrbigolin@disroot.org>
2025-04-05 00:27:41 -0300

    Implementa Ordered_List

--- src/car.cc
+++ src/car.cc
@@ -1,4 +1,27 @@
+#include "ordered_list.hh"
+#include <iostream>
+
 // Main temporária para não dar erro de compilação.
 int main() {
+  Ordered_List<int, int> k;
+  int v1 = 3;
+  int v2 = 1;
+  int v3 = 22;
+  k.insert(v1, 3);
+  k.insert(v2, 2);
+  k.insert(v2, 1);
+  k.insert(v3, 12);
+  k.insert(v3, 2);
+  k.insert(v1, 2);
+  k.insert(v3, 0);
+
+  k.remove(v3, 0);
+  k.remove(v2, 2);
+  k.remove(v3, 12);
+
+  for (Ordered_List<int, int>::Iterator it = k.begin(); it != k.end(); ++it) {
+    std::cout << it->value() << ' ' << it->rank() << '\n';
+  }
+
   return 0;
 }
