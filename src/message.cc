b6e8cace9f791dc9c9aa38c5356e92968cb792d9
Matheus Fernandes Bigolin <mfrdrbigolin@disroot.org>
2025-04-09 01:09:14 -0300

    Inicializa conteúdo de Message

--- src/message.cc
+++ src/message.cc
@@ -1,7 +1,10 @@
 #include "message.hh"
+#include <algorithm>
+#include <cstddef>
 
 Message::Message(std::size_t msg_size) : _size(msg_size) {
   _data = new std::byte[_size];
+  std::fill(_data, _data + _size, std::byte(0));
 }
 
 Message::~Message() {
