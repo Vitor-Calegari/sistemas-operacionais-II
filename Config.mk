b22c056d301e2468af0f3db86c8b6b5b76cdf7fe
Vitor Calegari <vitorprca@gmail.com>
2025-04-23 22:22:57 -0300

    Modifica nome do teste de api no make

--- Config.mk
+++ Config.mk
@@ -2,7 +2,7 @@ MAKENAME = INE5424
 
 INTERFACE_NAME:=$(shell ip addr | awk '/state UP/ {print $$2}' | head -n 1 | sed 's/.$$//')
 
-TESTS = communicator_test ping_pong_test load_test api_test
+TESTS = communicator_test ping_pong_test load_test latency_test
 MODULES = ethernet utils
 
 SRC_DIR = src
