c497daae02bc92a64b2502ea4d95e7eda57cd044
Matheus Fernandes Bigolin <mfrdrbigolin@disroot.org>
2025-05-09 21:21:03 -0300

    Versão inicial Communicator Publish-Subscribe

--- include/communicator.hh
+++ include/communicator.hh
@@ -2,8 +2,10 @@
 #define COMMUNICATOR_HH
 
 #include "concurrent_observer.hh"
+#include <vector>
 
-template <typename Channel, typename Message>
+// Publisher.
+template <typename Channel, typename Message, typename Transducer = void>
 class Communicator : public Concurrent_Observer<
                          typename Channel::Observer::Observed_Data,
                          typename Channel::Observer::Observing_Condition> {
@@ -14,11 +16,12 @@ class Communicator : public Concurrent_Observer<
 public:
   typedef typename Channel::Buffer Buffer;
   typedef typename Channel::Address Address;
-  typedef typename Channel::Port  Port;
+  typedef typename Channel::Port Port;
 
 public:
   Communicator(Channel *channel, Port port)
-      : _channel(channel), _address(Address(channel->getNICPAddr(), channel->getSysID(), port)) {
+      : _channel(channel),
+        _address(Address(channel->getNICPAddr(), channel->getSysID(), port)) {
     _channel->attach(this, _address.getPort());
   }
 
@@ -26,19 +29,76 @@ public:
     _channel->detach(this, _address.getPort());
   }
 
-  Address addr() { return _address; }
-
+  Address addr() {
+    return _address;
+  }
 
   bool send(Message *message) {
-    return _channel->send(*message->sourceAddr(), *message->destAddr(), message->data(),
-                          message->size()) > 0;
+    return _channel->send(*message->sourceAddr(), *message->destAddr(),
+                          message->data(), message->size()) > 0;
+  }
+
+private:
+  void update([[maybe_unused]] typename Channel::Observed *obs,
+              typename Channel::Observer::Observing_Condition c, Buffer *buf) {
+    // Releases the thread waiting for data.
+    Observer::update(c, buf);
+  }
+
+private:
+  Channel *_channel;
+  Address _address;
+
+  struct Subscriber {
+    Address origin;
+    int period;
+  };
+
+  std::vector<Subscriber> subscribers{};
+};
+
+// Subscriber.
+template <typename Channel, typename Message>
+class Communicator<Channel, Message, void>
+    : public Concurrent_Observer<
+          typename Channel::Observer::Observed_Data,
+          typename Channel::Observer::Observing_Condition> {
+  typedef Concurrent_Observer<typename Channel::Observer::Observed_Data,
+                              typename Channel::Observer::Observing_Condition>
+      Observer;
+
+public:
+  typedef typename Channel::Buffer Buffer;
+  typedef typename Channel::Address Address;
+  typedef typename Channel::Port Port;
+
+public:
+  Communicator(Channel *channel, Port port)
+      : _channel(channel),
+        _address(Address(channel->getNICPAddr(), channel->getSysID(), port)) {
+    _channel->attach(this, _address.getPort());
+  }
+
+  ~Communicator() {
+    _channel->detach(this, _address.getPort());
+  }
+
+  Address addr() {
+    return _address;
+  }
+
+  bool send(Message *message) {
+    return _channel->send(*message->sourceAddr(), *message->destAddr(),
+                          message->data(), message->size()) > 0;
   }
 
   bool receive(Message *message) {
     // Block until a notification is triggered.
     Buffer *buf = Observer::updated();
 
-    int size = _channel->receive(buf, message->sourceAddr(), message->destAddr(), message->data(), message->size());
+    int size =
+        _channel->receive(buf, message->sourceAddr(), message->destAddr(),
+                          message->data(), message->size());
     message->setSize(size);
 
     return size > 0;
