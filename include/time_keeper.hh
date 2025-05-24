d51d5cf151594c28cd5168222805bec6ad83bfb2
Vitor Calegari <vitorprca@gmail.com>
2025-05-24 13:03:16 -0300

    Implementa relógio simulado e implementa parcialmente desacoplamento do protocol

--- include/time_keeper.hh
+++ include/time_keeper.hh
@@ -1,12 +1,35 @@
 #include <atomic>
 #include <chrono>
+#include <condition_variable>
 #include <mutex>
 #include <semaphore>
 #include <sys/types.h>
 #include <thread>
 #include <vector>
-#include <condition_variable>
 
+class SimulatedClock {
+public:
+  SimulatedClock(uint64_t offset_ns = 0) : offset(offset_ns) {
+  }
+
+  // Define novo offset
+  void setOffset(uint64_t offset_ns) {
+    offset = offset_ns;
+  }
+
+  // Retorna offset atual
+  uint64_t getOffset() const {
+    return offset;
+  }
+
+  // Retorna tempo atual com offset aplicado
+  std::chrono::time_point<std::chrono::steady_clock> now() const {
+    return std::chrono::steady_clock::now() - std::chrono::nanoseconds(offset);
+  }
+
+private:
+  uint64_t offset; // em nanossegundos
+};
 
 template <typename Protocol>
 class SyncEngine {
@@ -16,82 +39,102 @@ public:
   static const ANNOUNCE = Protocol::PTP::ANNOUNCE;
   static const PTP = Protocol::PTP::PTP;
 
+  enum STATE { WAITING_DELAY = 0, WAITING_SYNC = 1 };
+
+  enum ACTION { DO_NOTHING = 0, SEND_DELAY_REQ = 1, SEND_DELAY = 2 };
+
 public:
   SyncEngine(Protocol *prot)
-      : _protocol(prot), _iamleader(0), _need_sync_period(1e6),
-        _need_sync_thread_running(false), _leader_period(1e6),
-        _leader_thread_running(false) {
-    startNeedSyncThread();
+      : _protocol(prot), _iamleader(0), _announce_period(1e6),
+        _announce_thread_running(false), _leader_period(1e6),
+        _leader_thread_running(false), _clock(0) {
+    startAnnounceThread();
     startLeaderThread();
   }
 
   ~SyncEngine() {
-    stopNeedSyncThread();
+    stopAnnounceThread();
     stopLeaderThread();
   }
 
+  // Função para lidar com a lógica do PTP. Cuida da logica do master,
+  // quanto do slave.
+  // return: int.
+  // 0: announce ou delay, não fazer nada.
+  // 1: sync, enviar delay req.
+  // 2: delay req, enviar delay.
+  // -1: erro
+  int handlePTP(uint32_t timestamp, Address origin_addr, uint8_t type) {
+    int ret = -1;
+    if (type == ANNOUNCE) { // All
+      addStratum(origin_addr.getSysID());
+      ret = ACTION::DO_NOTHING;
+    } else if (type == PTP && !_iamleader) { // Slave
+      if (origin_addr != _master_addr) {     // Sync de um lider diferente
+        // Anota tempos do PTP
+        _master_addr = origin_addr;
+        _sync_t = timestamp;
+        _recvd_sync_t = getTimestamp();
+        // Reset State Machine
+        _state = STATE::WAITING_DELAY;
+        ret = ACTION::SEND_DELAY_REQ;
+      } else {
+        if (_state == STATE::WAITING_SYNC) { // Sync
+          // Anota tempos do PTP
+          _sync_t = timestamp;
+          _recvd_sync_t = getTimestamp();
+          _state = STATE::WAITING_DELAY;
+          ret = ACTION::SEND_DELAY_REQ;
+        } else if (_state == STATE::WAITING_DELAY) { // Delay
+          // Calcula novo offset.
+          _leader_recvd_delay_req_t = timestamp;
+          // TODO AQUI O CALCULO CONSIDERA QUE T3 E T2 SÃO IGUAIS, POREM NA REALIDADE
+          // OS TEMPOS SERIAM DIFERENTES, POIS, T2 SERIA OBTIDO PELA PLACA DE REDE AO RECEBER
+          // SYNC E T3 SERIA INFORMADO PELA PLACA DE REDE AO REALMENTE ENVIAR A MENSAGEM.
+          uint64_t delay = ( (_leader_recvd_delay_req_t - _recvd_sync_t) + (_recvd_sync_t - _sync_t) ) / 2;
+          uint64_t offset = (_recvd_sync_t - _sync_t) - delay;
+          _clock.setOffset(offset);
+          _state = STATE::WAITING_SYNC;
+          ret = ACTION::DO_NOTHING;
+        }
+      }
+    } else if (type == PTP && _iamleader) { // Master
+      // Lider não tem maquina de estados
+      ret = ACTION::SEND_DELAY;
+    }
+    return ret;
+  }
+
+  uint64_t getTimestamp() const {
+    std::chrono::time_point<std::chrono::steady_clock> now = _clock.now();
+    return std::chrono::duration_cast<std::chrono::nanoseconds>(now.time_since_epoch()).count();
+  }
+
+private:
   void addStratum(Stratum stratum) {
-    std::lock_guard<std::mutex> lock(_strata_mutex);
     _knownStrata.push_back(stratum);
   }
 
   void clearStrata() {
-    std::lock_guard<std::mutex> lock(_strata_mutex);
     _knownStrata.clear();
   }
 
-  void handlePTP(uint32_t timestamp, Address origin_addr, uint8_t type) {
-    if (type == ANNOUNCE) { // All
-      addStratum(origin_addr.getSysID());
-    } else if (type == PTP && !_iamleader) {  // Slave
-      if (origin_addr != _master_addr) {
-        // Reset State Machine
-        _state = 0;
-        _master_addr = origin_addr;
-        // Anota tempos do PTP
-        _sync_t = timestamp;
-        _recvd_sync_t = 0; // TODO obter tempo de agora
-        // Envia Delay Req
-        Address myaddr = _protocol->getAddr();
-        _protocol->send(myaddr, origin_addr, PTP, nullptr, 0);
-      } else {
-        if (_state == 0) { // Sync
-          // Anota tempos do PTP
-          _sync_t = timestamp;
-          _recvd_sync_t = 0; // TODO obter tempo de agora
-          // Envia Delay Req
-          Address myaddr = _protocol->getAddr();
-          _protocol->send(myaddr, origin_addr, PTP, nullptr, 0);
-          _state++;
-        } else if (_state == 1) { // Delay
-          _leader_recvd_delay_req_t = timestamp;
-          // TODO atualizar seu relógio
-          _state = 0;
-        }
-      }
-    } else if (type == PTP && _iamleader) {  // Master
-        // Envia Delay
-        Address myaddr = _protocol->getAddr();
-        _protocol->send(myaddr, origin_addr, PTP, nullptr, 0);
-    }
-  }
-
-private:
-  void startNeedSyncThread() {
-    _need_sync_thread_running = true;
-    _need_sync_thread = std::thread([this]() {
-      while (_need_sync_thread_running) {
+  void startAnnounceThread() {
+    _announce_thread_running = true;
+    _announce_thread = std::thread([this]() {
+      while (_announce_thread_running) {
         // Anuncia que está na rede
         Address myaddr = _protocol->getAddr();
         Address broadcast = _protocol->getBroadcastAddr();
-        uint8_t type = Protocol::PTP::ANNOUNCE;
-        _protocol->send(myaddr, broadcast, type, nullptr, 0);
+        _protocol->send(myaddr, broadcast, ANNOUNCE, nullptr, 0);
         // Espera eventuais anuncios de outros veiculos
         auto next_wakeup_t = std::chrono::steady_clock::now() +
-                             std::chrono::seconds(_need_sync_period);
+                             std::chrono::seconds(_announce_period);
         std::this_thread::sleep_until(next_wakeup_t);
         // Verifica se é lider
-        bool _iamleader = amILeader();
+        _leader_mutex.lock();
+        _iamleader = amILeader();
+        _leader_mutex.unlock();
         clearStrata();
         // Se é lider, começa a mandar syncs periodicos
         // se não é, volta a esperar
@@ -100,11 +143,11 @@ private:
     });
   }
 
-  void stopNeedSyncThread() {
-    if (_need_sync_thread_running) {
-      _need_sync_thread_running = false;
-      if (_need_sync_thread.joinable()) {
-        _need_sync_thread.join();
+  void stopAnnounceThread() {
+    if (_announce_thread_running) {
+      _announce_thread_running = false;
+      if (_announce_thread.joinable()) {
+        _announce_thread.join();
       }
     }
   }
@@ -113,17 +156,18 @@ private:
     _leader_thread_running = true;
     _leader_thread = std::thread([this]() {
       while (_leader_thread_running) {
+        // Primeira iteração sempre bloqueia. Enquanto eu for lider, não
+        // bloqueia.
         std::unique_lock<std::mutex> lock(_leader_mutex);
-        // Primeira iteração sempre bloqueia. Enquanto eu for lider, não bloqueia.
         _leader_cv.wait(lock, [this]() { return _iamleader; });
+
         auto next_wakeup_t = std::chrono::steady_clock::now() +
                              std::chrono::seconds(_leader_period);
 
         // Envia Sync Broadcast
         Address myaddr = _protocol->getAddr();
         Address broadcast = _protocol->getBroadcastAddr();
-        uint8_t type = Protocol::PTP::PTP;
-        _protocol->send(myaddr, broadcast, type, nullptr, 0);
+        _protocol->send(myaddr, broadcast, PTP, nullptr, 0);
 
         std::this_thread::sleep_until(next_wakeup_t);
       }
@@ -142,14 +186,12 @@ private:
   bool amILeader() {
     Stratum myStratum = _protocol->getSysID();
     bool ret = true;
-    _strata_mutex.lock();
     for (const auto &stratum : _knownStrata) {
       if (stratum < myStratum) {
         ret = false;
         break;
       }
     }
-    _strata_mutex.unlock();
     return ret;
   }
 
@@ -157,12 +199,11 @@ private:
   Protocol *_protocol;
   bool _iamleader;
   // Stratum --------------------------------------
-  std::mutex _strata_mutex;
   std::vector<Stratum> _knownStrata{};
   // Need Sync Thread -----------------------------
-  uint32_t _need_sync_period;
-  std::thread _need_sync_thread;
-  std::atomic<bool> _need_sync_thread_running;
+  uint32_t _announce_period;
+  std::thread _announce_thread;
+  std::atomic<bool> _announce_thread_running;
   // Leader Thread --------------------------------
   std::thread _leader_thread;
   uint32_t _leader_period;
@@ -170,11 +211,11 @@ private:
   std::condition_variable _leader_cv;
   std::mutex _leader_mutex;
   // PTP ------------------------------------------
-  uint32_t _sync_t;
-  uint32_t _recvd_sync_t;
-  uint32_t _delay_req_t;
-  uint32_t _leader_recvd_delay_req_t;
+  uint64_t _sync_t;
+  uint64_t _recvd_sync_t;
+  uint64_t _delay_req_t;
+  uint64_t _leader_recvd_delay_req_t;
   Address _master_addr;
   int _state;
-  std::mutex _state_mutex;
+  SimulatedClock _clock;
 };
