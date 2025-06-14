9f27772dac8109011eab67f413bae143cdb8cc95
Matheus Fernandes Bigolin <mfrdrbigolin@disroot.org>
2025-06-14 13:43:30 -0300

    Implementa hierarquia de Message

--- include/data_frames/message.hh
+++ include/data_frames/message.hh
@@ -9,25 +9,25 @@
 #include <cstdint>
 
 template <typename Addr>
-class Message {
+class MessageCommon {
 public:
-  Message(Addr src, Addr dst, std::size_t payload_size,
-          Control ctrl = Control(Control::Type::COMMON))
-      : _source_addr(src), _dest_addr(dst), _ctrl(ctrl), _timestamp(0),
+  MessageCommon(Addr src, Addr dst, std::size_t payload_size,
+                Control ctrl = Control(Control::Type::COMMON))
+      : _source_addr(src), _dest_addr(dst), _ctrl(ctrl),
         _payload_size(payload_size) {
     _data = new std::byte[_payload_size];
     std::fill(_data, _data + _payload_size, std::byte(0));
   }
 
-  Message(std::size_t payload_size,
-          Control ctrl = Control(Control::Type::COMMON))
-      : _source_addr(Addr()), _dest_addr(Addr()), _ctrl(ctrl), _timestamp(0),
+  MessageCommon(std::size_t payload_size,
+                Control ctrl = Control(Control::Type::COMMON))
+      : _source_addr(Addr()), _dest_addr(Addr()), _ctrl(ctrl),
         _payload_size(payload_size) {
     _data = new std::byte[_payload_size];
     std::fill(_data, _data + _payload_size, std::byte(0));
   }
 
-  ~Message() {
+  virtual ~MessageCommon() {
     delete[] _data;
   }
 
@@ -46,6 +46,54 @@ public:
   Control *getControl() {
     return &_ctrl;
   }
+
+  void setSize(std::size_t new_size) {
+    _payload_size = new_size;
+  }
+
+  std::size_t size() {
+    return _payload_size;
+  }
+
+  std::byte *data() const {
+    return _data;
+  }
+
+  template <typename T>
+  T *data() {
+    return std::bit_cast<T *>(&_data);
+  }
+
+  virtual double *getCoordX() = 0;
+  virtual double *getCoordY() = 0;
+  virtual uint64_t *timestamp() = 0;
+  virtual MAC::Tag *tag() = 0;
+
+private:
+  Addr _source_addr;
+  Addr _dest_addr;
+  Control _ctrl;
+  std::size_t _payload_size;
+  std::byte *_data;
+} __attribute__((packed));
+
+template <typename Addr>
+class Message : public MessageCommon<Addr> {
+public:
+  using Base = MessageCommon<Addr>;
+
+  Message(Addr src, Addr dst, std::size_t payload_size,
+          Control ctrl = Control(Control::Type::COMMON))
+      : Base(src, dst, payload_size, ctrl), _timestamp(0) {
+  }
+
+  Message(std::size_t payload_size,
+          Control ctrl = Control(Control::Type::COMMON))
+      : Base(payload_size, ctrl), _timestamp(0) {
+  }
+
+  ~Message() = default;
+
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Waddress-of-packed-member"
   double *getCoordX() {
@@ -61,37 +109,50 @@ public:
   }
 #pragma GCC diagnostic pop
 
-  void setSize(std::size_t new_size) {
-    _payload_size = new_size;
-  }
-
-  std::size_t size() {
-    return _payload_size;
-  }
-
   MAC::Tag *tag() {
     return &_tag;
   }
 
-  std::byte *data() const {
-    return _data;
-  }
-
-  template <typename T>
-  T *data() {
-    return std::bit_cast<T *>(&_data);
-  }
-
 private:
-  Addr _source_addr;
-  Addr _dest_addr;
-  Control _ctrl;
   double _coord_x;
   double _coord_y;
   uint64_t _timestamp;
-  std::size_t _payload_size;
   MAC::Tag _tag{};
-  std::byte *_data;
+} __attribute__((packed));
+
+template <typename Addr>
+class IntraMessage : public MessageCommon<Addr> {
+public:
+  using Base = MessageCommon<Addr>;
+
+  IntraMessage(Addr src, Addr dst, std::size_t payload_size,
+               Control ctrl = Control(Control::Type::COMMON))
+      : Base(src, dst, payload_size, ctrl) {
+  }
+
+  IntraMessage(std::size_t payload_size,
+               Control ctrl = Control(Control::Type::COMMON))
+      : Base(payload_size, ctrl) {
+  }
+
+  ~IntraMessage() = default;
+
+  // TODO! Retornar algo nos campos abaixo.
+  double *getCoordX() {
+    return {};
+  }
+
+  double *getCoordY() {
+    return {};
+  }
+
+  uint64_t *timestamp() {
+    return {};
+  }
+
+  MAC::Tag *tag() {
+    return {};
+  }
 } __attribute__((packed));
 
 #endif
