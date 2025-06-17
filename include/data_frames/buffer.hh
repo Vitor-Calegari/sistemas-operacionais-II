53c7b226dd164dd5e6eb51fa4fa206a3d59919be
Matheus Fernandes Bigolin <mfrdrbigolin@disroot.org>
2025-06-17 14:06:42 -0300

    Modifica classe Buffer para comportar tipos diferentes

--- include/data_frames/buffer.hh
+++ include/data_frames/buffer.hh
@@ -1,10 +1,15 @@
 #ifndef BUFFER_HH
 #define BUFFER_HH
 
-template <typename Data>
-class Buffer : private Data {
+#include <cstddef>
+class Buffer {
 public:
-  Buffer() : _size(0), _max_size(0), _in_use(false) {
+  static constexpr size_t BUFFER_SIZE = 1514;
+
+  enum BufferType { EthernetFrame, LiteFrame };
+
+  Buffer(BufferType buf_type)
+      : _type(buf_type), _size(0), _max_size(0), _in_use(false) {
   }
 
   // Construtor: define a capacidade máxima do buffer.
@@ -14,13 +19,18 @@ public:
   }
 
   // Retorna um ponteiro para o objeto Data contido no buffer.
-  Data *data() {
-    return this;
+  template <typename T>
+  T *data() {
+    return reinterpret_cast<T *>(&_data);
   }
 
   // Retorna o tamanho atual dos dados válidos no buffer (em bytes).
   int size() {
     return _size;
+  }
+
+  BufferType type() const {
+    return _type;
   }
 
   // Define o tamanho atual dos dados válidos.
@@ -61,9 +71,11 @@ public:
   }
 
 private:
+  BufferType _type;
   int _size;     // Tamanho atual dos dados válidos
   int _max_size; // Capacidade máxima do buffer
   bool _in_use;  // Flag para gerenciamento em um pool de buffers
+  std::byte _data[BUFFER_SIZE];
 };
 
 #endif
